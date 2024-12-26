dd65da89d789408e3b1681982e39c883dbf4727d
fx900-fx97@7 <ss365493@gmail.com>
2024-12-26 23:07:18 +0530

    first commit

--- src/redux/features/preferenceSlice.ts
+++ src/redux/features/preferenceSlice.ts
@@ -25,40 +25,40 @@ export const preferenceSlice = createSlice({
   initialState,
   reducers: {
     setMenuArray: (state, action: PayloadAction<string[]>) => {
-      // console.log("Menu Array:", action.payload);
-      state.menuArray = action.payload; // Update the menuArray
+      
+      state.menuArray = action.payload; 
     },
     setMenu: (state, action: PayloadAction<string>) => {
-      // console.log("Payload:", action.payload);
+      
       state.menu = action.payload;
-      // console.log("Updated State:", JSON.parse(JSON.stringify(state)));
+      
     },
     setDropdown: (state, action: PayloadAction<string>) => {
-      // console.log("Payload:", action.payload);
+      
       state.dropdownType = action.payload;
-      // console.log("Updated State:", JSON.parse(JSON.stringify(state)));
+      
     },
     setSearch: (state, action: PayloadAction<string>) => {
       state.search = action.payload;
-      // console.log("Updated State:", JSON.parse(JSON.stringify(state)));
+      
     },
     setData: (state, action: PayloadAction<string[]>) => {
-      state.data = action.payload; // Replace the existing data with the new array
-      // console.log("Updated Data:", state.data);
+      state.data = action.payload; 
+      
     },
     setCategoryArray: (state, action: PayloadAction<string[]>) => {
-      // console.log("Menu Array:", action.payload);
-      state.categoryArray = action.payload; // Update the menuArray
+      
+      state.categoryArray = action.payload; 
     },
     setPopup: (state, action: PayloadAction<boolean>) => {
-      // console.log("Payload:", action.payload);
+      
       state.popup = action.payload;
-      // console.log("Updated State:", JSON.parse(JSON.stringify(state)));
+      
     },
   },
 });
 
-// Action creators are generated for each case reducer function
+
 export const { setMenuArray, setMenu,
   setDropdown,
   setSearch, setData,
